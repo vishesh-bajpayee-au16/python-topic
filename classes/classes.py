e7dbf80eff9c0d6b1bd65e31aa534227c671211c
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-05 13:42:20 +0530

    added inheretance

--- classes/classes.py
+++ classes/classes.py
@@ -1,10 +1,26 @@
 class Person:
-    def __init__(self,name,age):
-        self.name = name 
+    def __init__(self,firstname,lastname,age,number):
+        self.firstname = firstname
+        self.lastname = lastname
         self.age = age 
+        self.number = number 
 
-    def greet(self):
-        print(f"Hello my name is {self.name}")
+    def fullName(self):
+        print(f"FullName is: {self.firstname} {self.lastname}")
     
-    def getAge(self):
-        print(f"hello my age is: {self.age}")
+    def retirementAge(self):
+        print(f"Retirement age: {65 - self.age}")
+    
+
+person1 = Person("Vishesh","Bajpayee",22,7987852367)
+
+class Student(Person):
+    def __init__(self,firstname,lastname,age,number,marks):
+        super().__init__(firstname,lastname,age,number)
+        self.marks = marks
+
+    def marks(self):
+        return self.marks
+
+student1 = Student("vishu","bajpayee",22,123,99)
+print(student1.marks())
