d765d3d126e446ab6d8387823e4d79c5129cf4bd
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-08 00:00:28 +0530

    added questions problems solutions

---
+++ questions/qs01.py
@@ -0,0 +1,29 @@
+# QUESTION - 1 https://www.codechef.com/problems/FLOW001
+
+T = int(input())
+arr = []
+sumFinal = []
+
+def sumArr(elementCount):
+    for i in range(elementCount):
+        input_ = list(map(int,input().split()))
+        arr.append(input_)
+    return arr
+
+
+def finalArr(arr):
+    for i in arr:
+        total = i[0] + i[1]
+        sumFinal.append(total)
+    return sumFinal
+
+
+def ans(finalArr):
+    for i in finalArr:
+        print(i)
+
+
+arr1 = sumArr(T)
+arr2 = finalArr(arr1)
+ans(arr2)
+
