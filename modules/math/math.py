584cef8827509d9e9f741f335eea9236d97fcb83
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-05-07 20:08:14 +0530

    added modules

---
+++ modules/math/math.py
@@ -0,0 +1,28 @@
+import math
+
+
+# Built-in Math Functions that dont require math module import
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+z = -12.12
+
+
+print(x)
+print(y)
+print(abs(z))
+
+print(pow(4,3))
+
+
+# math module methods 
+
+# square root
+print(math.sqrt(16))
+
+# incriment to +1 intiger
+print(math.ceil(9.3))
+# decriment to -1 intiger
+print(math.floor(9.3))
+
+# pi value 
+print(math.pi)
